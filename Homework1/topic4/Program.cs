78181fac52d1c97fb8aaf5b7e42cdb59775ab56c
AsteriaChiang <AsteriaChiang@outlook.com>
2018-09-17 22:18:35 +0800

    homework1

---
+++ Homework1/topic4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace topic4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input the first number");
+            float a = float.Parse(Console.ReadLine());
+            Console.WriteLine("Please input the second number");
+            float b = float.Parse(Console.ReadLine());
+            float addNum = a + b;
+            Console.WriteLine("两大数相加结果为：{0}", addNum);
+        }
+    }
+}
