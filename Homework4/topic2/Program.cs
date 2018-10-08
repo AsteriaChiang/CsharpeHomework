f72fb839ee3d8f9b08a4aec995f60f21afc4d6f6
AsteriaChiang <AsteriaChiang@outlook.com>
2018-10-08 21:05:44 +0800

    homework4

--- Homework4/topic2/Program.cs
+++ Homework4/topic2/Program.cs
@@ -11,58 +11,129 @@ namespace topic2
         //订单：订单号，客户名称
         public class Order
         {
-            string orderNumber;
-            string clientName;
-            public void completeOrder()
+            private string number;
+            private string name;
+            public Order(string orderNumber, string clientName)
             {
-                Console.WriteLine("Please input the number of order");
-                orderNumber = Console.ReadLine();
-                Console.WriteLine("Please input the name of client");
-                clientName = Console.ReadLine();
+                this.number = orderNumber;
+                this.name = clientName ;
+            }
+
+            public string Number
+            {
+                get { return number; }
+            }
+
+            public string Name
+            {
+                get { return name; }
             }
     }
         //订单明细：商品、价格等细节
         public class OrderDetails
         {
-            string goodName;
-            float goodPrice;
-            int goodNumber;
-            float orderPrice;
-            public void completeOrder()
+            private string name;
+            private float price;
+            private int number;
+            private float total;
+            public OrderDetails(string goodName,float goodPrice, int goodNumber,float orderTotal)
             {
-                Console.WriteLine("Please input the name of good");
-                goodName = Console.ReadLine();
-                Console.WriteLine("Please input the price of good");
-                goodPrice = float.Parse(Console.ReadLine());
-                Console.WriteLine("Please input the number of good");
-                goodNumber = int.Parse(Console.ReadLine());
-                Console.WriteLine("Please input the price of order");
-                orderPrice = float.Parse(Console.ReadLine());
+                this.name = goodName;
+                this.price = goodPrice;
+                this.number = goodNumber;;
+                this.total = orderTotal;
+            }
+
+            public string Name
+            {
+                get { return name; }
+            }
+
+            public float Price
+            {
+                get { return price; }
+            }
+           
+            public int Number
+            {
+                get { return number; }
+            }
+
+            public float Total
+            {
+                get { return total; }
             }
         }
         //订单服务：添加、删除、修改、查询订单
         public class OrderService
         {
-            List<object> orderList = new List<object>();
+            List<Order> orderList = new List<Order>();
+            Order order;
+            OrderDetails details;
+            public OrderService(Order o, OrderDetails d)
+            {
+                this.order = o;
+                this.details = d;
+            }
+            //添加
             public void addOrder()
             {
-                
+                orderList.Add(order);
             }
-            public void deleteOrder()
+            //删除
+            public void deleteOrder(object o)
             {
-
+                for (int i = 0; i < orderList.Count; i++)
+                {
+                    if ((string)o==orderList[i].Name&& (string)o == orderList[i].Number)
+                    {
+                        orderList.RemoveAt(i);
+                    }
+                }
             }
-            public void changeOrder()
+            //修改
+            public void changeOrder(object o,Order change)
             {
-
+                for (int i = 0; i < orderList.Count; i++)
+                {
+                    if ((string)o == orderList[i].Name && (string)o == orderList[i].Number)
+                    {
+                        orderList[i] = change;
+                    }
+                }
             }
-            public void searchOrder()
+            //查找
+            public Order searchOrder(object o)
             {
-
+                int order=0;
+                for (int i = 0; i < orderList.Count; i++)
+                {
+                    if ((string)o == orderList[i].Name && (string)o == orderList[i].Number)
+                    {
+                        order = i;
+                    }
+                }
+                return orderList[order];
             }
         }
         static void Main(string[] args)
         {
+            Console.WriteLine("请输入订单的订单号");
+            string number = Console.ReadLine();
+            Console.WriteLine("请输入订单的顾客名");
+            string name = Console.ReadLine();
+            Console.WriteLine("请输入订单包含的商品");
+            string gname = Console.ReadLine();
+            Console.WriteLine("请输入订单的顾客名");
+            float price = float.Parse(Console.ReadLine());
+            Console.WriteLine("请输入订单的顾客名");
+            int gnumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("请输入订单的顾客名");
+            float total = float.Parse(Console.ReadLine());
+            Order order = new Order(number,name);
+            OrderDetails details = new OrderDetails(gname, price, gnumber, total);
+            OrderService service = new OrderService(order,details);
+            service.addOrder();
         }
     }
 }
