5e6258c509fb8c4340ef51d46236806b248ee8a9
AsteriaChiang <AsteriaChiang@outlook.com>
2018-10-22 22:11:52 +0800

    homework6

---
+++ Homework6/topic1/Mainn.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace topic1
+{
+    class Mainn
+    {
+        public static void Main()
+        {
+            try
+            {
+
+                Customer customer1 = new Customer(1, "Customer1");
+
+                Goods milk = new Goods(1, "Milk", 69);
+                Goods eggs = new Goods(2, "eggs", 4);
+                Goods apple = new Goods(3, "apple", 5);
+
+                OrderDetail orderDetails1 = new OrderDetail(1, apple, 8);
+                OrderDetail orderDetails2 = new OrderDetail(2, eggs, 2);
+                OrderDetail orderDetails3 = new OrderDetail(3, milk, 1);
+
+                Order order1 = new Order(1, customer1);
+
+                order1.AddDetails(orderDetails1);
+                order1.AddDetails(orderDetails2);
+                order1.AddDetails(orderDetails3);
+                //order1.AddOrderDetails(orderDetails3);
+
+
+                OrderService os = new OrderService();
+                os.AddOrder(order1);
+                os.Export(order1);
+
+                Console.WriteLine("GetAllOrders");
+                List<Order> orders = os.QueryAllOrders();
+                foreach (Order od in orders)
+                    Console.WriteLine(od.ToString());
+
+                Console.WriteLine("GetOrdersByCustomerName:'Customer2'");
+                orders = os.QueryByCustomerName("Customer2");
+                foreach (Order od in orders)
+                    Console.WriteLine(od.ToString());
+
+                Console.WriteLine("GetOrdersByGoodsName:'apple'");
+                orders = os.QueryByGoodsName("apple");
+                foreach (Order od in orders)
+                    Console.WriteLine(od.ToString());
+
+                Console.WriteLine("Remove order(id=2) and qurey all");
+                os.RemoveOrder(2);
+                os.QueryAllOrders().ForEach(
+                    od => Console.WriteLine(od));
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
---
+++ Homework6/topic1/OrderService.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+using System.Runtime.Serialization;
+
+
+namespace topic1
+{
+    /// 实现添加订单、删除订单、修改订单、查询订单（按照订单号、商品名称、客户等字段进行查询)
+    class OrderService
+    {
+
+        public Dictionary<uint, Order> orderDict;
+        public OrderService()
+        {
+            orderDict = new Dictionary<uint, Order>();
+        }
+
+        //添加订单
+        public void AddOrder(Order order)
+        {
+            if (orderDict.ContainsKey(order.Id))
+                throw new Exception($"order-{order.Id} is already existed!");
+            orderDict[order.Id] = order;
+        }
+
+        //删除订单
+        public void RemoveOrder(uint orderId)
+        {
+            orderDict.Remove(orderId);
+        }
+
+        //修改订单
+        public List<Order> QueryAllOrders()
+        {
+            return orderDict.Values.ToList();
+        }
+
+        //查询订单
+        public Order GetById(uint orderId)
+        {
+            return orderDict[orderId];
+        }
+
+        //序列化为XML
+        public void Export(Order order)
+        {
+           
+            XmlSerializer xmlser = new XmlSerializer(typeof(List<Order>));
+            String xmlFileName = "C://Users//Astoria//Desktop//order.xml";
+            FileStream fs = new FileStream(xmlFileName, FileMode.Create,FileAccess.ReadWrite);
+            xmlser.Serialize(fs, order);
+            fs.Close();
+
+        }
+        
+        //从XML文件中载入订单
+        public void Import()
+        {
+            String xmlFileName = "C://Users//Astoria//Desktop//order.xml";
+            FileStream fs = new FileStream(xmlFileName, FileMode.Open);
+            XmlSerializer xmlser = new XmlSerializer(typeof(List<Order>));
+            Order order = xmlser.Deserialize(fs) as Order;
+
+            AddOrder(order);
+        }
+
+        public List<Order> QueryByGoodsName(string goodsName)
+        {
+            List<Order> result = new List<Order>();
+            foreach (Order order in orderDict.Values)
+            {
+                var goods = from OrderDetail in order.Details where OrderDetail.Goods.Name == goodsName select order;
+                foreach (var n in goods)
+                {
+                    result.Add(n);
+                }
+            }
+            return result;
+        }
+
+        public List<Order> QueryByCustomerName(string customerName)
+        {
+            var goods = orderDict.Values.Where(order => order.Customer.Name == customerName);
+            return goods.ToList();
+        }
+
+    }
+}
